5eb38e8a48d630aee42ea8d36ae666e6a6377ada
literature <65801567+LiteraturePro@users.noreply.github.com>
2022-02-04 12:02:38 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App"
 import { BrowserRouter } from "react-router-dom"
 import { RealmProvider } from "./contexts/Realm"
 
-const appId = process.env.REACT_APP_REALM_APP_ID
+const appId = "domain-monitor-updar"
 
 if (typeof appId !== "undefined") {
   ReactDOM.render(
